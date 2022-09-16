96803cebfdf08f598d7e6be414b843806b7e23e5
Yousef-Essam <Yousef-Essam>
2022-09-16 02:40:22 +0200

    Modified Tests for Orders Endpoints

--- src/tests/02-handlers/03-ordersHandlerSpec.ts
+++ src/tests/02-handlers/03-ordersHandlerSpec.ts
@@ -139,13 +139,13 @@ describe('Testing Orders Endpoints', () => {
             expect(response.status).toBe(401);
         })
 
-        it('should fail because there is no order with this id', async () => {
+        it('should fail because there is no order with id of 3', async () => {
             const response = await request.get('/orders/3/finish').set('Authorization', 'Bearer ' + await token.importToken());
 
             expect(response.status).toBe(404);
         })
 
-        it('should successfully show the active orders of the user', async () => {
+        it('should successfully finish the order of id 1', async () => {
             const response = await request.get('/orders/1/finish').set('Authorization', 'Bearer ' + await token.importToken());
 
             expect(response.status).toBe(200);
@@ -165,6 +165,71 @@ describe('Testing Orders Endpoints', () => {
                         price: 1000,
                         category: 'category2',
                         quantity: 1
+                    }
+                ]
+            })
+        })
+
+        it('should be forbidden since the order is already complete', async () => {
+            const response = await request.get('/orders/1/finish').set('Authorization', 'Bearer ' + await token.importToken());
+
+            expect(response.status).toBe(403);
+        })
+    })
+
+    describe('Testing Show Certain Order Endpoint', () => {
+        it('should fail for requiring the Authorization Token', async () => {
+            const response = await request.get('/orders/1');
+
+            expect(response.status).toBe(401);
+        })
+
+        it('should fail because there is no order with id of 3', async () => {
+            const response = await request.get('/orders/3').set('Authorization', 'Bearer ' + await token.importToken());
+
+            expect(response.status).toBe(404);
+        })
+
+        it('should successfully show the order of id 1', async () => {
+            const response = await request.get('/orders/1').set('Authorization', 'Bearer ' + await token.importToken());
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual({
+                order_id: 1,
+                status: 'complete',
+                products: [
+                    {
+                        id: 2,
+                        name: 'Product2',
+                        price: 750,
+                        category: 'category1',
+                        quantity: 2
+                    },
+                    {
+                        id: 3,
+                        name: 'Product3',
+                        price: 1000,
+                        category: 'category2',
+                        quantity: 1
+                    }
+                ]
+            })
+        })
+
+        it('should successfully show the order of id 2', async () => {
+            const response = await request.get('/orders/2').set('Authorization', 'Bearer ' + await token.importToken());
+
+            expect(response.status).toBe(200);
+            expect(response.body).toEqual({
+                order_id: 2,
+                status: 'active',
+                products: [
+                    {
+                        id: 1,
+                        name: 'Product1',
+                        price: 500,
+                        category: 'category1',
+                        quantity: 3
                     }
                 ]
             })
